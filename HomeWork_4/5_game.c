c2ac585528877d6c4d7342cd762b220770d494ad
rougeedge <rougeedge@mail.ru>
2014-12-10 09:53:36 +0300

    mistake in 5_game

--- HomeWork_4/5_game.c
+++ HomeWork_4/5_game.c
@@ -8,29 +8,25 @@ int main()
     int answer;
     do
     {
-        int my_number;
+        int my_number = 0;
         int computer_number = rand()%99 + 1;
-        int amount_of_step = 1;
-        do
+        int amount_of_step = 0;
+        while(my_number != computer_number)
         {
             printf("Enter a number from 1 to 99: ",my_number);
             scanf("%d",&my_number);
-            if(my_number == computer_number)
-            {
-                break;
-            }
             amount_of_step += 1;
 
             if(my_number > computer_number)
             {
                 printf("This number MORE then need!\n");
             }
-            else
+            else if(my_number < computer_number)
             {
                 printf("This number LESS then need!\n");
             }
 
-            } while(my_number != computer_number);
+        }
             printf("Congratulations! Your amount of steps = %d \n",amount_of_step);
 
             printf("Do you wont to repeat the game?(1-yes/0-no) ");
