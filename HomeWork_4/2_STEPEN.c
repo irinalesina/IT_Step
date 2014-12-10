9dbe39567ae65567de6e889faee315462bc563f7
rougeedge <rougeedge@mail.ru>
2014-12-10 09:28:27 +0300

    mistake in 2_STEPEN

--- HomeWork_4/2_STEPEN.c
+++ HomeWork_4/2_STEPEN.c
@@ -3,13 +3,25 @@
 
 int main()
 {
-    int x;
+    int x, i = 1, stepen = 1;
     printf("Enter a number x = ");
     scanf("%d", &x);
     signed int p;
     printf("Enter a stage of number p = ");
     scanf("%d", &p);
-    printf("%f", pow(x,p));
+    for(i; i <= abs(p); i++)
+    {
+        stepen *= x;
+    }
+    if(p < 0)
+    {
+        printf("%f",1.0/stepen);
+    }
+    else
+    {
+        printf("%d",stepen);
+    }
+
 
     return 0;
 }
