ee8cf5ec6586a2ca5526a56c61e1bc3623b5d6ec
irinalesina <irina.lesina1995@gmail.com>
2014-12-10 08:17:51 +0300

    my mistake

--- HomeWork_4/5_game.c
+++ HomeWork_4/5_game.c
@@ -5,32 +5,38 @@
 int main()
 {
     srand(time(NULL));
-
-    int my_number;
-    int computer_number = rand()%100;
-    int amount_of_step = 1;
+    int answer;
     do
     {
-        printf("Enter a nomber from 0 to 99: ",my_number);
-        scanf("%d",&my_number);
-        if(my_number == computer_number)
+        int my_number;
+        int computer_number = rand()%99 + 1;
+        int amount_of_step = 1;
+        do
         {
-            break;
-        }
-        amount_of_step += 1;
+            printf("Enter a number from 1 to 99: ",my_number);
+            scanf("%d",&my_number);
+            if(my_number == computer_number)
+            {
+                break;
+            }
+            amount_of_step += 1;
 
-        if(my_number > computer_number)
-        {
-            printf("This number MORE then need!\n");
-        }
-        else
-        {
-            printf("This number LESS then need!\n");
-        }
+            if(my_number > computer_number)
+            {
+                printf("This number MORE then need!\n");
+            }
+            else
+            {
+                printf("This number LESS then need!\n");
+            }
 
-    } while(my_number != computer_number);
+            } while(my_number != computer_number);
+            printf("Congratulations! Your amount of steps = %d \n",amount_of_step);
 
-    printf("You amount of steps = %d \n",amount_of_step);
+            printf("Do you wont to repeat the game?(1-yes/0-no) ");
+            scanf("%d", &answer);
+
+    } while(answer);
 
     return 0;
 }
