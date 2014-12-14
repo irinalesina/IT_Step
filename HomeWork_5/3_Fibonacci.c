a3bf54f75592bc47acba8c121a4238a48f47e61a
irinalesina <irina.lesina1995@gmail.com>
2014-12-14 13:28:44 +0300

    bug in 3

--- HomeWork_5/3_Fibonacci.c
+++ HomeWork_5/3_Fibonacci.c
@@ -3,14 +3,15 @@
 
 int main()
 {
-    unsigned int a1 = 1, a2 = 0, a3, i = 1;
+    unsigned int a1 = 1, a2 = 1, a3, i = 1;
     for(i; i <= 40 ; i++)
     {
         a3 = a2 + a1;
-        printf("%f\t%f\n ",1.0*a2/a1, 1.0*a1/a2);
-
+        printf("%f\t%f\n", 1.0*a2/a1, 1.0*a1/a2);
         a1 = a2;
         a2 = a3;
+
+
     }
 
     return 0;
