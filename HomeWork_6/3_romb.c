09a2c18f4c7f7bb7ba8c6dddebaf9440a97c0037
irinalesina <irina.lesina1995@gmail.com>
2014-12-16 23:22:38 +0300

    hw6

---
+++ HomeWork_6/3_romb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int diagonal;
+    printf("Enter diagonal = ");
+    scanf("%d", &diagonal);
+    int i, j;
+    for(i = 1; i <= diagonal; i++)
+    {
+       for(j = 1; j <= diagonal; j++)
+       {
+            if((i + j == diagonal/2+2) || (j - i == diagonal/2) ||
+               (i - j == diagonal/2) || (i + j == diagonal + diagonal/2 + 1))
+            {
+                printf("#");
+            }
+            else
+            {
+                printf(" ");
+            }
+       }
+       printf("\n");
+    }
+
+    return 0;
+}
