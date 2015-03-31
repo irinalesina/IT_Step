d9bcd313302f59307c2df562fbb116ebe9e80063
irinalesina <irina.lesina1995@gmail.com>
2015-03-31 23:06:18 +0300

    Gregorian calendar

--- 20_Fraction_ComplexNumb_Time_Date_DataBase/5_date/main.c
+++ 20_Fraction_ComplexNumb_Time_Date_DataBase/5_date/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "date.h"
 
 int main()
 {
-    printf("Hello world!\n");
+    Date date_1, date_2;
+
+    Input(&date_1);
+    Output(date_1);
+
+    Input(&date_2);
+    Output(date_2);
+
+    printf("Difference between dates = %d\n", DifferenceBetweenDates(date_1, date_2));
     return 0;
 }
