98df377d4b00cafe440eb890809c261e61bc0f32
irinalesina <irina.lesina1995@gmail.com>
2015-04-30 12:09:37 +0000

    graph

--- 22_RPN_Graf_PlanProcess_Labyrinth/2_Graf/main.c
+++ 22_RPN_Graf_PlanProcess_Labyrinth/2_Graf/main.c
@@ -4,21 +4,30 @@
 
 
 typedef struct NodeOfList_
-    {
-        int number_of_node;
-        struct NodeOfList_ *link;
-    } NodeOfList;
+{
+    int number_of_node;
+    struct NodeOfList_ *link;
+} NodeOfList;
 
 
 int main()
 {
     FILE *graph_test;
     graph_test = fopen("graph_test.txt", "r");
+
     if(graph_test == NULL)
     {
         printf("File not open!\n");
         exit(1);
     }
+    int size_of_file;
+    fseek(graph_test, 0, SEEK_END);
+    size_of_file = ftell(graph_test);
+    fseek(graph_test, 0, SEEK_SET);
+    int *data = (int *)malloc(size_of_file * sizeof(int));
+
+    memcpy(data, graph_test, size_of_file);
+
 
 
 
