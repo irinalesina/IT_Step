f9d765234e5b0f6d342d1eae4c65f7b3642dc128
irinalesina <irina.lesina1995@gmail.com>
2015-05-12 20:16:21 +0300

    final project

---
+++ MyFinalProject/TestUniversalStack/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../UniversalStack_lib/universal_stack.h"
+
+int main()
+{
+    int arr[7] = {1, 2, 3, 4, 5, 6, 7};
+    UniversalStack test;
+    Initialize(&test, sizeof(int), 10);
+    int i;
+    for(i = 0; i < 7; i++)
+    {
+        Push(&test, &arr[i]);
+    }
+    int element;
+    OnTop(&test, &element);
+    printf("Element on top = %d\n", element);
+    while(!IsEmpty(&test))
+    {
+        Pop(&test, &element);
+        printf("%d ", element);
+    }
+    Uninitialize(&test);
+
+    return 0;
+}
---
+++ MyFinalProject/UniversalStack_lib/universal_stack.h
@@ -0,0 +1,20 @@
+#ifndef STACK_F_H_INCLUDED
+#define STACK_F_H_INCLUDED
+
+typedef struct _UniversalStack
+{
+    void *stack_memory;
+    int element_size;
+    int element_max_count;
+    int top;
+}UniversalStack;
+
+int Initialize(UniversalStack *us, int element_size, int element_max_count);//инициализация динамич массива под стек//bool
+void Uninitialize(UniversalStack *us);//очистка памяти
+
+int Push(UniversalStack *us, void *element);//bool
+int Pop(UniversalStack *us, void *element);//bool
+int OnTop(UniversalStack *us, void *element);
+int IsEmpty(UniversalStack *us);//bool
+
+#endif // STACK_F_H_INCLUDED
